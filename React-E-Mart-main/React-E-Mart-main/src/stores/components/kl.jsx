9ad5bc49788aebb49ed922c3ae544f21edeead59
SAIPRADYUMNA7 <2210030348@klh.edu.in>
2025-04-03 13:58:59 +0530

    reacccrt

---
+++ React-E-Mart-main/React-E-Mart-main/src/stores/components/kl.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function KL() {
+  return (
+    <div>
+      <h1>K L University</h1>
+      <img src='img/image.png' alt="KL University Logo" />
+    </div>
+  );
+}
+
+export default KL;
